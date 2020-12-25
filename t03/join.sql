b3a6816fb52049ab6f7eaaaeb6a969c2b14bf47b
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-25 18:59:36 +0200

    edited

--- t03/join.sql
+++ t03/join.sql
@@ -1,10 +1,10 @@
 -- get all the heroes with their team name. Should be displayed as NULL if the hero hasno team
-SELECT * FROM heroes LEFT OUTER JOIN teams
+SELECT heroes.name, teams.name FROM heroes LEFT OUTER JOIN teams
 ON (heroes.id = teams.hero_id);
 
-SELECT * FROM heroes LEFT OUTER JOIN powers
+SELECT heroes.name, powers.name FROM heroes LEFT OUTER JOIN powers
 ON (heroes.id = powers.hero_id);
 
-SELECT * FROM heroes 
+SELECT heroes.name, powers.name FROM heroes 
 INNER JOIN teams ON (heroes.id = teams.hero_id)
 INNER JOIN powers ON (heroes.id = powers.hero_id);
